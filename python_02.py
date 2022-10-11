bd2d806cebebc83c6138a8cf733d13087122e88a
PRATHAM SAHU <80022922+pratham-0094@users.noreply.github.com>
2022-10-11 18:56:30 +0530

    video 3

---
+++ python_02.py
@@ -0,0 +1,41 @@
+# Variables are used to store data, they take memory space based on the type of value we assigning to them.
+# Example
+var1 = "hello "  # string
+var4 = "20.3"  # string
+var5 = "world"  # string
+var2 = 10  # int
+var3 = 20.5  # float
+
+print(var1+var5)
+print(10*"hello\t")  # print particular string 10 times
+print(10*str(float(var4)+var3))
+
+print(int(var3))
+print(float(var2))
+# str() converts the specified value into a string
+# int() converts the specified value into a integer
+# float() converts the specified value into a float
+
+print(type(var1))
+# type() used to get the type of an object
+
+var9 = True  # bool
+print(type(var9))
+
+# Datatypes in Python
+
+# --numeric
+# interger
+# float
+# complex number
+
+# --sequence
+# string
+# list
+# tuple
+
+# --boolean
+
+# --dictionary
+
+# --set
