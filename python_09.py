86a0337242cce06cf28444b17837e1209e4474a2
PRATHAM SAHU <80022922+pratham-0094@users.noreply.github.com>
2022-11-29 19:08:54 +0530

    video 10

---
+++ python_09.py
@@ -0,0 +1,94 @@
+# Types of Operator
+# Arithmetic Operator
+# Assignment Operator
+# Comparsion Operator
+# Identity Operator
+# Membership Operator
+# Logical Operator
+# Bitwise Operator
+
+
+# Arithmetic Operator
+print("8 + 2 is ", 8+2)
+print("8 - 2 is ", 8-2)
+print("8 * 2 is ", 8*2)
+print("8 / 2 is ", 8/2)
+print("8 // 2 is ", 8//2)
+print("8 ** 2 is ", 8**2)
+print("8 % 2 is ", 8 % 2)
+
+
+# Assignment Operator
+val1 = 10
+print(val1)
+val1 += 10  # val1 = val1 + 10
+print(val1)
+val1 -= 10  # val1 = val1 - 10
+print(val1)
+val1 *= 10  # val1 = val1 * 10
+print(val1)
+val1 /= 10  # val1 = val1 / 10
+print(val1)
+val1 //= 10  # val1 = val1 // 10
+print(val1)
+val1 **= 10  # val1 = val1 ** 10
+print(val1)
+val1 %= 10  # val1 = val1 % 10
+print(val1)
+# other assignment Operator
+# &=
+# |=
+# ^=
+# <<=
+# >>=
+
+
+# Comparsion Operator
+v1 = 10
+v2 = 10
+print(v1 == v2)
+print(v1 != v2)
+print(v1 < v2)
+print(v1 > v2)
+print(v1 <= v2)
+print(v1 >= v2)
+
+
+# Logical Operator
+print(True and True)
+print(False and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(False or True)
+print(True or False)
+print(False or False)
+print(not True)
+print(not False)
+
+
+# Identity Operator
+a = 1
+b = 2
+print(a is b)
+print(a is not b)
+
+
+# Membership Operator
+l1 = [1, 2, 3, 4, 5]
+print(1 in l1)
+print(10 not in l1)
+
+
+# Bitwise Operator
+# 0 - 00
+# 1 - 01
+# 2 - 10
+# 3 - 11
+print(3 & 2)
+print(0 | 2)
+# other bitwise Operator
+# ~
+# ^
+# <<
+# >>
